a21c214854a377f5be2e8df6e5ae2983c1bbdc9d
Mahdi Masoumi <mahdimasoumi3232@gmail.com>
2023-08-31 13:11:14 +0330

    SinglePageHome_ver1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,17 +17,13 @@ function Navbar( { theme , setTheme , isNavigate , newHome } ) {
 
     const navRef = useRef(null);
 
-    useEffect(()=>{
-
-        if (isNavigate == true) {
-            // navRef.current.style.width = '80%';
-        }else {
-            // navRef.current.style.width = '100%';
+    function handleClickScroll( itemId ){
+        if (itemId) {
+          const element = document.querySelector(`#${itemId}`)
+          element.scrollIntoView({ behavior: 'smooth' });
         }
+    }
 
-    } , [ isNavigate ])
-    
-    console.log(123);
     function setTh() {
         if (themeColor == 'light') {
             setTheme('dark')
@@ -53,20 +49,20 @@ function Navbar( { theme , setTheme , isNavigate , newHome } ) {
     
     const NavItems = [
         {
-            title : "Home",
-            href : "/"
+            title : "What's Bebin",
+            itemId : 'home-whatIsBebin'
+        },
+        {
+            title : "Features",
+            itemId : 'home-features'
         },
         {
             title : "Pricing",
-            href : "/pricing"
+            itemId : 'home-pricing'
         },
         {
-            title : "Download",
-            href : "/download"
-        },
-        {
-            title : "About",
-            href : "/about"
+            title : "Critics",
+            itemId : 'home-critics'
         }
     ]
     
@@ -78,15 +74,16 @@ function Navbar( { theme , setTheme , isNavigate , newHome } ) {
             setLoginToggle(false)
         }
     }
+    
 
   return (
         <div ref={navRef} className={` h-[70px] px-10 flex justify-between items-center  rounded-full border-2 border-black dark:border-zinc-600 transition-[width] duration-500 ${newHome ? `${isNavigate ? 'w-5/6 bg-white bg-opacity-95 dark:bg-zinc-900 dark:bg-opacity-80 border-opacity-10 dark:border-opacity-50  backdrop-blur-lg dark:backdrop-blur-xl' : 'w-full  border-opacity-0 dark:border-opacity-0 bg-none dark:bg-none'}` : `${isNavigate ? 'w-5/6 bg-white bg-opacity-90 dark:bg-zinc-900 dark:bg-opacity-80 border-opacity-10 dark:border-opacity-50  backdrop-blur-lg dark:backdrop-blur-xl' : 'w-full  border-opacity-0 dark:border-opacity-0 bg-none dark:bg-none'}` } `}>
             <div>
-                <Link to="/" className={`text-3xl font-bold ${newHome ? `${isNavigate ? 'text-black dark:text-white' : 'text-white'}` : ``}`}>Be<span className='text-amber-500'>Bin</span></Link>
+                <Link onClick={() => handleClickScroll('homePage')} to="/" className={`text-3xl font-bold ${newHome ? `${isNavigate ? 'text-black dark:text-white' : 'text-white'}` : ``}`}>Be<span className='text-amber-500'>Bin</span></Link>
             </div>
             <ul className='flex gap-4 font-medium'>
-                {NavItems.map((item , index)=>{
-                    return <li key={index} className='hover:scale-105 transition-transform duration-300'><Link className={`px-3 py-4 rounded-full ${newHome ? `${isNavigate ? 'opacity-100 hover:bg-opacity-30 hover:bg-amber-200 dark:hover:bg-white dark:hover:bg-opacity-10' : 'text-white opacity-90 hover:bg-white hover:bg-opacity-20'}` : 'opacity-100 hover:bg-amber-200 hover:bg-opacity-20 dark:hover:bg-white dark:hover:bg-opacity-10' } hover:opacity-90 `} to={item.href}>{item.title}</Link></li>
+                {NavItems.map(( item , index )=>{
+                    return <li key={index} className='hover:scale-105 transition-transform duration-300'><button onClick={() => handleClickScroll(item.itemId) } className={`px-3 py-4 rounded-full ${newHome ? `${isNavigate ? 'opacity-100 hover:bg-opacity-30 hover:bg-amber-200 dark:hover:bg-white dark:hover:bg-opacity-10' : 'text-white opacity-90 hover:bg-white hover:bg-opacity-20'}` : 'opacity-100 hover:bg-amber-200 hover:bg-opacity-20 dark:hover:bg-white dark:hover:bg-opacity-10' } hover:opacity-90 `} >{item.title}</button></li>
                 })}
             </ul>
 
@@ -97,7 +94,7 @@ function Navbar( { theme , setTheme , isNavigate , newHome } ) {
                     <button className='cursor-pointer text-violet-600 hover:text-yellow-400 transition-all hover:bg-yellow-500 hover:bg-opacity-10 px-2 py-2 rounded-full' onClick={setTh}><LightModeIcon /></button>
                 }
                 
-                <Mainbtn title="Join Now !" customFunction={openTogglerCustom} clickAnimation={true} size="medium" ColorsContext={ColorsContext}/>
+                <Mainbtn title="Join Now !" customFunction={() => {openTogglerCustom()}} clickAnimation={true} size="medium" ColorsContext={ColorsContext}/>
                 {loginToggle == true ? <div className='absolute right-0 top-16'>
                     <Toggler items={ListenNowItems} setIsOpen={setLoginToggle}/> 
                 </div> : null}
