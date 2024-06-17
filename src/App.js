40fd17fa5f6bf598fd307c7fe599050096d84eab
Mehdi Masoumi <dev.mahdimasoumi@gmail.com>
2024-06-17 14:03:11 +0330

    Final

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Layout from './pages/Layout';
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Layout from "./pages/Layout";
 
 function App() {
-
-  const [ theme , setTheme ] = useState(null);
+  const [theme, setTheme] = useState(null);
   const ThemeContext = createContext(theme);
 
-  useEffect(()=>{
-
-    if (localStorage.getItem('theme') == undefined ) {
-
-      localStorage.setItem('theme', 'light');
-      setTheme('light');
-    }else {
-
-      setTheme(localStorage.getItem('theme'))
+  useEffect(() => {
+    if (localStorage.getItem("theme") === undefined) {
+      localStorage.setItem("theme", "light");
+      setTheme("light");
+    } else {
+      setTheme(localStorage.getItem("theme"));
     }
 
-    if (theme == 'dark') {
-      document.documentElement.classList.add('dark');
-    }else if (theme == 'light') {
-      document.documentElement.classList.remove('dark');
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else if (theme === "light") {
+      document.documentElement.classList.remove("dark");
     }
-
-  } , [ theme ] )
+  }, [theme]);
 
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout theme={ThemeContext} setTheme={setTheme}/>}>
-            <Route index element={<Home ThemeContext={ThemeContext}/>} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout theme={ThemeContext} setTheme={setTheme} />}
+        >
+          <Route index element={<Home ThemeContext={ThemeContext} />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
